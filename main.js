bc6804a286835f8c88cc350795d26f186f07c138
Won Maung Thein <wonmaungthein@gmail.com>
2018-09-07 22:20:33 +0100

    improved the styling

---
+++ main.js
@@ -0,0 +1,8 @@
+// function changeColor(newColor) {
+//   var title = document.getElementByClassName("donateBike");
+//   title.style.color = newColor;
+// }
+
+// function myFunction() {
+//   alert("I am an alert box!");
+// }
